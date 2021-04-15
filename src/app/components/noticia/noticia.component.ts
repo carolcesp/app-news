ba9153eaf9de527796933399bf1f1a2671cddd17
carol cespedes <carolinacespedes90@gmail.com>
2021-04-15 10:32:08 +0200

    click al detalle

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
@@ -11,9 +13,13 @@ export class NoticiaComponent implements OnInit {
   @Input('datosNoticia') noticia: Article;
   @Input() indice: number
 
-  constructor() { }
+  constructor(private iab: InAppBrowser) { }
 
   ngOnInit() {
+  }
+
+  openNews() {
+    const browser = this.iab.create(this.noticia.url,'_system');
   }
 
 }
